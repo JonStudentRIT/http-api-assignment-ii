908278c262578de0ec81e169a5a871e50c0b6325
JonStudentRIT <jk9927@g.rit.edu>
2023-02-15 14:17:42 -0500

    Server loads, basic file structure added, htmlResponses added and tested

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
+
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const onRequest = (request, response) => {
+  htmlHandler.getIndex(request, response);
+};
+
+http.createServer(onRequest).listen(port);
