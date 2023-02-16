411614bc974466d2258811fca4a0a55c4550759b
JonStudentRIT <jk9927@g.rit.edu>
2023-02-15 20:02:06 -0500

    dont really see the point of the entire html responses file, file has been refactored into a single function

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -1,21 +1,39 @@
+// const fs = require('fs');
+// const { request } = require('http');
+
+// const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+// const css = fs.readFileSync(`${__dirname}/../client/style.css`);
+
+// const wrightResponse = (response, fileType, type) => {
+//   response.writeHead(200, { 'Content-Type': type });
+//   response.write(fileType);
+//   response.end();
+// };
+
+// // get the home page
+// const getIndex = (request, response) => {
+//   wrightResponse(response, index, 'text/html');
+// };
+
+// // get the css
+// const getCSS = (request, response) => {
+//   wrightResponse(response, css, 'text/css');
+// };
+
+// module.exports = {
+//   getIndex,
+//   getCSS
+// };
+
 const fs = require('fs');
+//const { request } = require('http');
 
-const index = fs.readFileSync(`${__dirname}/../client/client.html`);
-const css = fs.readFileSync(`${__dirname}/../client/style.css`);
-
-// get the home page
-const getIndex = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(index);
+const loadhtml = (request, response, path, type) => {
+  response.writeHead(200, { 'Content-Type': type });
+  response.write(fs.readFileSync(`${__dirname}/../${path}`));
   response.end();
-};
+}
 
-// get the css
-const getCSS = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/css' });
-  response.write(css);
-  response.end();
+module.exports = {
+  loadhtml,
 };
-
-module.exports.getIndex = getIndex;
-module.exports.getCSS = getCSS;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const url = require('url');
-const query = require('querystring');
+//const query = require('querystring');
 
 const htmlHandler = require('./htmlResponses.js');
 const jsonHandler = require('./jsonResponses.js');
@@ -8,7 +8,8 @@ const jsonHandler = require('./jsonResponses.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const onRequest = (request, response) => {
-  htmlHandler.getIndex(request, response);
+  //htmlHandler.getIndex(request, response);
+  htmlHandler.loadhtml(request, response, 'client/client.html', 'text/html');
 };
 
 http.createServer(onRequest).listen(port);
