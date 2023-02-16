a4f46bd78b2ed04006d4823f732788caf27a10a2
JonStudentRIT <jk9927@g.rit.edu>
2023-02-15 23:09:29 -0500

    Get Users and Update users now functional, server onRequest now takes parameters, page not found and missing parameter messages added

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,5 +1,12 @@
 // object to hold users
-const users = {};
+const users = {
+  users: {
+    // Jon: {
+    //   name: 'Jon',
+    //   age: 123,
+    // },
+  },
+};
 
 // general json response get function
 const respondJSON = (request, response, status, object) => {
@@ -17,15 +24,53 @@ const respondJSONMeta = (request, response, status) => {
 };
 
 // get list of users
-const getUsers = (request, response) => respondJSON(request, response, 200, object);
+const getUsers = (request, response) => respondJSON(request, response, 200, users);
 // get list of users without the users
 const getUsersMeta = (request, response) => respondJSONMeta(request, response, 200);
 // update existing user
-const updateUser = (request, response) => respondJSON(request, response, 201, object);
+const updateUser = (request, response, params) => {
+  // test changes
+  // users.users.Jon.name = 'Karcher';
+  // users.users.Jon.age = 321;
+  // const newUser = {
+  //     name: 'New User',
+  //     age: 456,
+  // }
+  // users.users['New User'] = newUser;
+  let responseJSON = {};
+  if (!params.name || !params.age) {
+    responseJSON = {
+      message: 'Name and age are both required.',
+      id: 'addUserMissingParams',
+    };
+    return respondJSON(request, response, 400, responseJSON);
+  }
+  //   if (users.users[params.name]) {
+  //     users.users[params.name].name = params.name;
+  //     users.users[params.name].age = params.age;
+  //   }
+
+  const newUser = {
+    name: params.name,
+    age: params.age,
+  };
+  users.users[params.name] = newUser;
+  responseJSON = users;
+
+  return respondJSON(request, response, 201, responseJSON);
+};
 // page not found
-const notFound = (request, response) => respondJSON(request, response, 404, object);
+const notFound = (request, response) => {
+  const responseJSON = {
+    message: 'The page you are looking for was not found.',
+    id: 'notFound',
+  };
+  respondJSON(request, response, 404, responseJSON);
+};
 // page not found without a message
-const notFoundMeta = (request, response) => respondJSONMeta(request, response, 404);
+const notFoundMeta = (request, response) => {
+  respondJSONMeta(request, response, 404);
+};
 
 module.exports = {
   getUsers,
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const url = require('url');
+const query = require('querystring');
 
 const htmlHandler = require('./htmlResponses.js');
 const jsonHandler = require('./jsonResponses.js');
@@ -8,29 +9,34 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 // list of all handled URLs by method
 const urlStruct = {
-  'GET': {
-    '/': htmlHandler.getIndex,
-    '/getUsers': htmlHandler.getIndex,
-    '/notReal': htmlHandler.getIndex,
-    '/addUser': htmlHandler.getIndex,
-    '/style.css': htmlHandler.getCSS,
-    notFound: jsonHandler.notFound,
-  },
-  'HEAD': {
-    '/getUsers': jsonHandler.getUsersMeta,
-    notFound: jsonHandler.notFoundMeta,
-  },
+  // 'GET': {
+  '/': htmlHandler.getIndex,
+  '/getUsers': jsonHandler.getUsers,
+  '/notReal': htmlHandler.getIndex,
+  '/addUser': jsonHandler.updateUser,
+  '/style.css': htmlHandler.getCSS,
+  notFound: jsonHandler.notFound,
+  // },
+  // 'HEAD': {
+  //   '/getUsers': jsonHandler.getUsersMeta,
+  //   notFound: jsonHandler.notFoundMeta,
+  // },
 };
 
 // get the url
 const onRequest = (request, response) => {
   const parsedUrl = url.parse(request.url);
+  const params = query.parse(parsedUrl.query);
   console.log(parsedUrl);
 
   // not found meta goes first
 
   // assuming the path exists
-  urlStruct['GET'][parsedUrl.pathname](request, response);
+  if (urlStruct[parsedUrl.pathname]) {
+    urlStruct[parsedUrl.pathname](request, response, params);
+  } else {
+    urlStruct.notFound(request, response);
+  }
 };
 
 http.createServer(onRequest).listen(port);
